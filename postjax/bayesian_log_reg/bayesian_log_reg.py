a44f1e2794f4caebfab6062a404ce0e33f391037
Bernardo Williams <williwilliams3@hotmail.com>
2023-12-27 11:58:35 +0100

    Update bayesian_log_reg.py

--- postjax/bayesian_log_reg/bayesian_log_reg.py
+++ postjax/bayesian_log_reg/bayesian_log_reg.py
@@ -1,34 +1,54 @@
-
+import os
 import numpy as np
 import jax.numpy as jnp
-from jax.nn import softplus
+from jax.nn import sigmoid
+import jax.scipy.stats as jss
+
+
+current_file_path = os.path.abspath(__file__)
+current_directory = os.path.dirname(os.path.dirname(current_file_path))
+
 
 ########################################################
 # Model from RMHMC paper
 class baylogreg:
     def __init__(self):
         self.name = "BayLogReg"
-        self.alpha = 1.0
         self.data = self.load_data()
         self.D = self.data["D"]
+        self.alpha_var = 100.0  # prior variance
+        self.standardized = True
 
-    def load_data(self):
-        data = np.loadtxt("postjax/data/australian.dat")
-        A = data[:, :-1]
-        A = (A - jnp.mean(A, axis=0)) / jnp.std(A, axis=0)
-        A = jnp.hstack([jnp.ones((len(A), 1)), A])
+    def load_data(self, file_name="data/australian.npy"):
+        data = np.load(os.path.join(current_directory, file_name))
+        X = data[:, :-1]
+        if self.standardized:
+            X = (X - jnp.mean(X, axis=0)) / jnp.std(X, axis=0)
+        new_x = np.ones((X.shape[0], 1))
+        X = jnp.asarray(np.concatenate([X, new_x], axis=1))
         y = data[:, -1]
-        N, D = A.shape
-        return dict(N=N, D=D, X=A, y=y)
+        assert jnp.all((y == 0) | (y == 1))
+        N, D = X.shape
+        return dict(N=N, D=D, X=X, y=y)
 
-    def logp(self, x):
+    def logp(self, theta):
         # No prior
-
+        sqrt_alpha = jnp.sqrt(self.alpha_var)
         data = self.data
-        N = data["N"]
         X = data["X"]
         y = data["y"]
 
-        assert len(x) == self.D
-        mu = jnp.dot(X, x)
-        return jnp.dot(y, mu) - jnp.sum(softplus(mu))
+        assert len(theta) == self.D
+        return jnp.sum(jss.norm.logpdf(theta, 0.0, sqrt_alpha)) + jnp.sum(
+            jss.bernoulli.logpmf(y, sigmoid(jnp.dot(X, theta)))
+        )
+
+    def fisher_metric_fn(self, theta):
+        data = self.data
+        X = data["X"]
+        dim = self.D
+        alpha = self.alpha_vav
+        preds = sigmoid(jnp.dot(X, theta))
+        # Inspired by https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.linalg.svd.html
+        metric = (X.T * preds * (1.0 - preds)) @ X + jnp.eye(dim) / alpha
+        return 0.5 * (metric + metric.T)

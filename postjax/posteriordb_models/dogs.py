Fix numerical issues

postjax/posteriordb_models/dogs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import jax.scipy.stats as jss
-from jax.nn import sigmoid
+from jax.nn import sigmoid, softplus
+import jax.numpy as jnp
 from .utils import get_posterior
 
 
@@ -10,9 +11,9 @@ class dogs_dogs:
         self.name = "dogs-dogs"
         self.posterior = get_posterior(self.name, pdb_path)
         self.data = self.posterior.data.values()
+        self.clipping_tolerance = 1e-5
 
     def logp(self, x):
-        # ['mu.1', 'mu.2', 'sigma.1', 'sigma.2', 'theta']
         if len(x) != 3:
             raise ("Input must be 3 dim vector")
         data = self.data
@@ -23,8 +24,14 @@ class dogs_dogs:
         n_shock[:, 1:] += np.cumsum(y[:, :-1], axis=1)
 
         p = x[0] + x[1] * n_avoid + x[2] * n_shock
+
+        sigma = sigmoid(p)
+        sigma_clip = jnp.clip(
+            sigma, self.clipping_tolerance, 1.0 - self.clipping_tolerance
+        )
+
         return (
-            jss.bernoulli.logpmf(y, p=sigmoid(p)).sum()
+            jss.bernoulli.logpmf(y, sigma_clip).sum()
             + jss.norm.logpdf(x, loc=0.0, scale=100).sum()
         )
 
@@ -35,9 +42,9 @@ class dogs_log:
         self.name = "dogs-dogs_log"
         self.posterior = get_posterior(self.name, pdb_path)
         self.data = self.posterior.data.values()
+        self.clipping_tolerance = 1e-5
 
     def logp(self, x):
-        # ['mu.1', 'mu.2', 'sigma.1', 'sigma.2', 'theta']
         if len(x) != 2:
             raise ("Input must be 2 dim vector")
         data = self.data
@@ -46,12 +53,22 @@ class dogs_log:
         n_shock = np.zeros((data["n_dogs"], data["n_trials"]))
         n_avoid[:, 1:] += np.cumsum(1 - y[:, :-1], axis=1)
         n_shock[:, 1:] += np.cumsum(y[:, :-1], axis=1)
-
-        p = sigmoid(x[0] * n_avoid + x[1] * n_shock)
+        # Transformation
+        y0 = (x[0] - 100) / 100
+        y1 = x[1] / 100
+        sigma0 = sigmoid(y0)
+        sigma1 = sigmoid(y1)
+        p = sigmoid(sigma0 * n_avoid + sigma1 * n_shock)
+        p_clip = jnp.clip(p, self.clipping_tolerance, 1.0 - self.clipping_tolerance)
         return (
             # likelihood
-            jss.bernoulli.logpmf(y, p=p).sum()
-            # prior
-            + jss.uniform.logpdf(x[0], loc=-100, scale=100)
-            + jss.uniform.logpdf(x[1], loc=0, scale=100)
+            jss.bernoulli.logpmf(y, p=p_clip).sum()
+            # prior is always zero
+            # + jss.uniform.logpdf(sigma1)
+            # + jss.uniform.logpdf(sigma2)
+            # change of variables
+            - softplus(-y0)
+            - softplus(y0)
+            - softplus(-y1)
+            - softplus(y1)
         )